f54a06160f43218402a1977c136eeeced41e88a6
Abdullah Barhoum <ka70911@gmail.com>
2019-09-11 14:12:32 +0200

    simplify the parser

--- satsolver/SATParser.py
+++ satsolver/SATParser.py
@@ -1,28 +1,25 @@
 from functools import reduce
 
-def parse(pathToFile, sort=True, log=True):
+COMMENT_CHARS = {"c", "%", "0"}
+
+def parse(pathToFile, log=True):
   minisatFile = open(pathToFile, "r").read().split('\n')
   numVariables = 0
   numClauses = 0
   clauses = []
 
-
-  i = 0
   for line in minisatFile:
-    if len(line) == 0 or line[0] in ["c","%","0"]: continue
+    if len(line) == 0 or line[0] in COMMENT_CHARS:
+      continue
 
     if line[0] == "p":
       lineContent = line.split()
       numVariables = int(lineContent[2])
       numClauses = int(lineContent[3])
-      clauses = [[] for n in range(numClauses)]
       continue
 
-    variables = map(int, line.split()[:-1])
-    clauses[i].extend(variables)
-    if sort:
-      clauses[i].sort()
-    i += 1
+    variables = list(set(map(int, line.split()[:-1])))
+    clauses.append(variables)
 
   assert numVariables > 0 and numClauses > 0 and reduce(lambda a, b: (a) + len(b), clauses, 0) > 0
 
