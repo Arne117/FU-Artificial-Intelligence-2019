2d427f1c610e304749a102deb82d0e6860641f4a
Linus <linus.helfmann@fu-berlin.de>
2019-09-12 14:27:36 +0200

    ein paar optimierungsversuche

--- satsolver/Solver.py
+++ satsolver/Solver.py
@@ -27,20 +27,20 @@ def solve(clauses, solutions):
       print2(solutions, "Solved")
       return True, solutions
 
+    goToNext = False
+
     # fail / backtrack
     for c in clauses:
+      # fail / backtrack
       if len(c) == 0:
         print2(solutions, "invalid branch")
         return False, None
-        # return...
 
-    # Unit propagation
-    goToNext = False
-    for c in clauses:
-      if len(c) == 1:
+      # Unit propagation
+      elif len(c) == 1:
         goToNext = True
         val = c[0]
-        print2(solutions,"Unit propagation", val)
+        print2(solutions, "Unit propagation", val)
         solutions.append(val)
         solveForVar(clauses, val)
         break
@@ -51,6 +51,7 @@ def solve(clauses, solutions):
     # Pure Literal
     # anzahl treffer
     found = {}
+    maxv = 0
     # vorzeichen -1 => -, 1 = +, 0 => beides => nicht gültig
     sign = {}
     for c in clauses:
@@ -66,6 +67,15 @@ def solve(clauses, solutions):
           found[key] = 1
           sign[key] = -1 + isPositive * 2
 
+        # das v mit maximalem Vorkommen
+        # oder das key mit max Vorkommen, da wir auch -v testen?
+        # bei unsat ist nach key besser, bei sat nach v (Unterschied max. 10-20%)
+        # im vergleich zu v für Split zufällig wählen:
+        # bei unsat ist v konkret wählen gut (Faktor 1/2) (Unsat2)
+        # bei sat ist es schlecht? (Faktor 5) (Sat4)
+        if maxv == 0 or found[key] > found[maxv]:
+          maxv = key
+
     for num, isPositive in sign.items():
       val = int(num) * isPositive
       print2(solutions, "found pure Literal", val)
@@ -77,37 +87,26 @@ def solve(clauses, solutions):
       continue
 
     # Split
-    tested = {}
-    for c in clauses:
-      for v in c:
-        key = abs(v)
-        if key in tested:
-          continue
-        tested[key] = 1
-        copyOne = deepcopy(clauses)
-        print2(solutions,"split", v)
-        solveForVar(copyOne, v)
-        isSolved, subSolutions = solve(copyOne, solutions+[v])
-        if isSolved:
-          solutions.append(v)
-          return True, subSolutions
-        
-        v = -v
-        copyTwo = deepcopy(clauses)
-        print2(solutions,"split", v)
-        solveForVar(copyTwo, v)
-        isSolved, subSolutions = solve(copyTwo, solutions+[v])
-        if isSolved:
-          solutions.append(v)
-          return True, subSolutions
+    v = maxv # oder: clauses[0][0]
+    copyOne = deepcopy(clauses)
+    print2(solutions,"split", v)
+    solveForVar(copyOne, v)
+    isSolved, subSolutions = solve(copyOne, solutions+[v])
+    if isSolved:
+      solutions.append(v)
+      return True, subSolutions
+    v = -v
+    copyTwo = deepcopy(clauses)
+    print2(solutions,"split", v)
+    solveForVar(copyTwo, v)
+    isSolved, subSolutions = solve(copyTwo, solutions+[v])
+    if isSolved:
+      solutions.append(v)
+      return True, subSolutions
 
-        # falls wir v und -v getestet haben, dann müssen wir keine weiteren v's prüfen
-        # da diese auch irgentwann v oder -v setzen oder wenn sie ohne v/-v auskommen,
-        # müssten sie auch mit v/-v funktionieren
-        # => ein durchgang reicht!
-        print2(solutions, "split v/-v didn't work => invalid branch")
-        return False, None
-
-    # alles probiert, nichts geht => fail
-    print2(solutions, "no split worked => invalid branch")
+    # falls wir v und -v getestet haben, dann müssen wir keine weiteren v's prüfen
+    # da diese auch irgentwann v oder -v setzen oder wenn sie ohne v/-v auskommen,
+    # müssten sie auch mit v/-v funktionieren
+    # => ein durchgang reicht!
+    print2(solutions, "split v/-v didn't work => invalid branch")
     return False, None
